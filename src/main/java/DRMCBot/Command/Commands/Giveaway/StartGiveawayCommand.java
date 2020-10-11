7836bb57ed03358e711dcaf9a078632611bb2864
Kayxue <s0990006@gmail.com>
2020-10-11 23:27:25 +0800

    2020-10-11

--- src/main/java/DRMCBot/Command/Commands/Giveaway/StartGiveawayCommand.java
+++ src/main/java/DRMCBot/Command/Commands/Giveaway/StartGiveawayCommand.java
@@ -84,11 +84,8 @@ public class StartGiveawayCommand implements ICommand {
                                     Matcher matcher = timeFormat.matcher(giveawayLength);
                                     long sec = 0;
                                     String winnercount;
-                                    String timeString = ctx.getArgs().get(0);
-                                    int lastTwoArgunentIndex = ctx.getMessage().getContentRaw().indexOf(timeString) + timeString.length();
-                                    String lastTwoArguments = ctx.getMessage().getContentRaw().substring(lastTwoArgunentIndex).trim();
-                                    List<String> lastArguments = Arrays.asList(lastTwoArguments.split(" "));
-                                    winnercount = lastArguments.get(0);
+                                    winnercount = ctx.getArgs().get(1);
+                                    String price = ctx.getMessage().getContentRaw().substring(ctx.getMessage().getContentRaw().indexOf(ctx.getArgs().get(2)));
                                     while (matcher.find()) {
                                         switch (matcher.group(2)) {
                                             case "d" -> sec += (Integer.parseInt(matcher.group(1)) * 86400);
@@ -123,6 +120,7 @@ public class StartGiveawayCommand implements ICommand {
         public boolean toRoll = true;
         public Member GiveawayCreator;
         public String CreatedDay;
+        public String EndDay;
 
         public GiveawayRunner(long second, TextChannel channel, int winnerCount, String price, Member creator) {
             this.Second = second;
@@ -140,10 +138,12 @@ public class StartGiveawayCommand implements ICommand {
             long s = ((Second % 86400) % 3600) % 60;
             Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone(ZoneId.of("Asia/Taipei")));
             CreatedDay = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(calendar.getTime()));
+            calendar.add(Calendar.SECOND, (int) Second);
+            EndDay = (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(calendar.getTime()));
             EmbedBuilder embedBuilder = EmbedUtils.defaultEmbed()
                     .setTitle(":tada: 抽獎已開始！")
                     .setDescription("**抽獎獎項：**" + price + "\n剩餘時間：" + d + "天" + h + "小時" + m + "分" + s + "秒")
-                    .setFooter("此抽獎由" + GiveawayCreator.getUser().getAsTag() + "舉辦\n" + "開始於：" + CreatedDay + "（GMT+08:00）");
+                    .setFooter("由" + GiveawayCreator.getUser().getAsTag() + "舉辦\n" + "將結束於：" + EndDay + " (GMT+08:00)", GiveawayCreator.getUser().getAvatarUrl());
             GiveawayMessage = GiveawayMessageChannel.sendMessage(embedBuilder.build()).complete();
             GiveawayMessage.addReaction(toReactGiveawayEmoji).complete();
             CacheList.RunningGiveaway.put(GiveawayMessageChannel.getId() + GiveawayMessage.getId(), Second);
@@ -161,7 +161,8 @@ public class StartGiveawayCommand implements ICommand {
                     long s = ((Second % 86400) % 3600) % 60;
                     EmbedBuilder embedBuilder = EmbedUtils.defaultEmbed()
                             .setTitle(":tada: 抽獎已開始！")
-                            .setDescription("**抽獎獎項：**" + price + "\n剩餘時間：" + d + "天" + h + "小時" + m + "分" + s + "秒");
+                            .setDescription("**抽獎獎項：**" + price + "\n剩餘時間：" + d + "天" + h + "小時" + m + "分" + s + "秒")
+                            .setFooter("由" + GiveawayCreator.getUser().getAsTag() + "舉辦\n" + "將結束於：" + EndDay + " (GMT+08:00)", GiveawayCreator.getUser().getAvatarUrl());
                     GiveawayMessage.editMessage(embedBuilder.build()).queue();
                 }
                 if (!CacheList.RunningGiveaway.containsKey(GiveawayMessageChannel.getId() + GiveawayMessage.getId())) {
@@ -186,6 +187,7 @@ public class StartGiveawayCommand implements ICommand {
                 EmbedBuilder embedBuilder = EmbedUtils.defaultEmbed()
                         .setTitle(":tada: 抽獎已圓滿結束！")
                         .setDescription("**抽獎獎項：**" + price + "\n剩餘時間：" + d + "天" + h + "小時" + m + "分" + s + "秒")
+                        .setFooter("由" + GiveawayCreator.getUser().getAsTag() + "舉辦\n" + "已結束於：" + EndDay + " (GMT+08:00)", GiveawayCreator.getUser().getAvatarUrl())
                         .setColor(0x0DFC3D);
                 GiveawayMessage.editMessage(embedBuilder.build()).queue();
 
